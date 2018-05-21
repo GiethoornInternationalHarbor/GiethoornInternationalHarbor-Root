1a663f69a75c918f2a47ceea7c552f3f0fd6d3a2
Marco <marcohavermans+github@hotmail.nl>
2018-05-21 15:56:49 +0200

    Updated deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,2 @@
-echo 'Start docker-compose down'
-docker-compose down
-echo 'End docker-compose down'
-
-echo 'Start docker-compose pull'
-docker-compose pull
-echo 'End docker-compose pull'
-
-echo 'Start docker-compose up'
-docker-compose up -d
-echo 'End docker-compose up'
+echo 'Running docker stack deploy'
+docker stack deploy -c docker-compose.yml
