83844d4a4553d600c6340578d053107ca3585787
Marco <marcohavermans+github@hotmail.nl>
2018-05-21 14:46:48 +0200

    Added -i parameter in travis.yml

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,11 @@
+echo 'Start docker-compose down'
 docker-compose down
+echo 'End docker-compose down'
+
+echo 'Start docker-compose pull'
 docker-compose pull
+echo 'End docker-compose pull'
+
+echo 'Start docker-compose up'
 docker-compose up -d
+echo 'End docker-compose up'
